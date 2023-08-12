89db2d60e0ab33175659a644de6daf97a7d1dbe4
DaN <danilosm721@gmail.com>
2023-08-12 11:36:53 -0300

    desafio finalizado

--- src/caixa-da-lanchonete.js
+++ src/caixa-da-lanchonete.js
@@ -1,25 +1,24 @@
 class CaixaDaLanchonete {
-
-    constructor() {
+    constructor() { // define os produtos e as formas de pagamento
+        this.metodosDePagamento = ['dinheiro', 'credito', 'debito'];
         this.cardapio = {
-            cafe: 3.0,
-            chantily: 1.5,
-            suco: 6.2,
-            sanduiche: 6.5,
-            queijo: 2.0,
-            salgado: 7.25,
-            combo1: 9.5,
-            combo2: 7.5,
+            'cafe': 3.00,
+            'chantily': 1.50,
+            'suco': 6.20,
+            'sanduiche': 6.50,
+            'queijo': 2.00,
+            'salgado': 7.25,
+            'combo1': 9.50,
+            'combo2': 7.50
         };
-        this.metodosDePagamento = ['dinheiro', 'debito', 'credito'];
     }
 
     calcularValorDaCompra(metodoDePagamento, itens) {
-        if (!this.metodosDePagamento.includes(metodoDePagamento)) {
+        if (!this.metodosDePagamento.includes(metodoDePagamento)) { // forma de pagamento não listada
             return "Forma de pagamento inválida!";
         }
 
-        if (itens.length === 0) {
+        if (itens.length === 0) { // sem itens no pedido
             return "Não há itens no carrinho de compra!";
         }
 
@@ -30,19 +29,21 @@ class CaixaDaLanchonete {
             let [produto, quantidade] = item.split(',');
             let preco = this.cardapio[produto];
 
-            if (!(produto in this.cardapio)) {
+            if (!(produto in this.cardapio)) { // item não listado
                 return "Item inválido!";
             }
 
-            if (quantidade <= 0) {
+            if (quantidade <= 0) { // item sem quantidade
                 return "Quantidade inválida!";
             }
 
-            produtos.push([produto, quantidade]);
+            produtos.push({ produto, quantidade });
             total += preco * quantidade;
         }
 
-        // Adicionar a verificação de principal + extra
+        // verificação de principal + extra
+        if (!this.verificaPedido(produtos))
+            return "Item extra não pode ser pedido sem o principal";
 
         if (metodoDePagamento === 'dinheiro') {
             total *= 0.95; // desconto de 5%
@@ -53,6 +54,19 @@ class CaixaDaLanchonete {
         return this.formataValor(total);
     }
 
+    formataValor(valor) {
+        return "R$ " + valor.toFixed(2).replace('.', ',');
+    }
+
+    possuiExtra(itens) {
+        for (let i = 0; i < itens.length; i++) {
+            let produto = itens[i].produto;
+            if (this.ehItemExtra(produto) === 1 || this.ehItemExtra(produto) === 2)
+                return true;
+        }
+        return false;
+    }
+    // se um item é um extra
     ehItemExtra(item) {
         if (item === 'chantily')
             return 1;
@@ -60,13 +74,34 @@ class CaixaDaLanchonete {
             return 2;
         return 0;
     }
+    // verifica se o extra está acompanhado de seu principal
+    verificaPedido(itens) {
+        let possui_cafe = false;
+        let possui_sanduiche = false;
 
-    ehItemCombo(item) {
-        return item === 'combo1' || item === 'combo2';
-    }
+        if (!this.possuiExtra(itens)) // verifica se existe algum item extra no pedido
+            return true;
 
-    formataValor(valor) {
-        return "R$ " + valor.toFixed(2).replace('.', ',');
+        for (let i = 0; i < itens.length; i++) { // verifica se o principal está incluso no pedido
+            let produto = itens[i].produto;
+            if (this.ehItemExtra(produto) === 1) {
+                for (let j = 0; j < itens.length; j++) {
+                    if (itens[j].produto === 'cafe')
+                        possui_cafe = true;
+                }
+                if (!possui_cafe)
+                    return false;
+            }
+            if (this.ehItemExtra(produto) === 2) {
+                for (let j = 0; j < itens.length; j++) {
+                    if (itens[j].produto === 'sanduiche')
+                        possui_sanduiche = true;
+                }
+                if (!possui_sanduiche)
+                    return false;
+            }
+        }
+        return true;
     }
 }
 
